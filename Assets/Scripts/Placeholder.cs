43f28451a9d88581eb093b549532b492c7d16158
Zenophran <zenophran@gmail.com>
2013-05-10 00:25:27 +1000

    Added a placeholder script to make sure the system's working

---
+++ Assets/Scripts/Placeholder.cs
@@ -0,0 +1,17 @@
+// This is a placeholder script to make sure git integration is working properly
+
+using UnityEngine;
+using System.Collections;
+
+public class Placeholder : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
